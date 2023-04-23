23630fde96aa4b3249fda7ca26166a0161a12818
nyakiomaina <carolinenyakio21@email.com>
2023-04-23 21:26:03 +0300

    fixes

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,38 +1,27 @@
-use risc0_zkvm::guest::GuestInput;
-use risc0_zkvm::prover::Prover;
+use risc0_zkvm::Prover;
 use risc0_zkvm::serde::{from_slice, to_vec};
 use sha2::{Digest, Sha256};
-use methods::{CHECK_SUBSTRING_ELF, CHECK_SUBSTRING_ID};
 
-#![no_std]
-#![feature(lang_items)]
-
-#[lang = "panic_impl"]
-#[no_mangle]
-pub fn panic_impl(_: &core::panic::PanicInfo<'_>) -> ! {
-    loop {}
-}
-
+use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
 
 fn main() {
-    let input = "Hello rust world";
-    let substring = "hello";
+    let input = "She, called Nyakio, went to the beach";
+    let substring = "Nyakio";
     let hash = Sha256::digest(input.as_bytes());
     let hash_hex = hex::encode(hash);
 
-    let mut prover = Prover::new(CHECK_SUBSTRING_ELF)
+    let mut prover = Prover::new(METHOD_NAME_ELF)
         .expect("Prover should be constructed from valid method source code and corresponding image ID");
-
-    prover.add_input_string(&substring);
-    prover.add_input_string(&input);
-    prover.add_input_string(&hash_hex);
-
+    prover.add_input_u32_slice(to_vec(substring).unwrap().as_slice());
+    prover.add_input_u32_slice(to_vec(input).unwrap().as_slice());
+    prover.add_input_u32_slice(to_vec(&hash_hex).unwrap().as_slice());
+    
     let receipt = prover
         .run()
         .expect("Code should be provable unless it had an error or overflowed the maximum cycle count");
 
     receipt
-        .verify(CHECK_SUBSTRING_ID)
+        .verify(&METHOD_NAME_ID)
         .expect("Code you have proven should successfully verify; did you specify the correct image ID?");
 
     let result: bool = from_slice(&receipt.journal).unwrap();
